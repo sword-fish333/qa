63aa5b27968c7b5e8d0c1926026b08ea0e4e9d36
Alin <ghiurcaalin@gmail.com>
2018-11-02 12:37:35 +0200

    Generated Question model via artisan

---
+++ app/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+class Question extends Model
+{
+    protected $fillable=['title','body'];
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=str_slug($value);
+    }
+}
